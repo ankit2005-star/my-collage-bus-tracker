049a7883f6839d7a680445d16850d7d834d02906
ankitstar-2005 <ankitgodbole90@gmail.com>
2025-02-24 17:03:11 +0530

    Removed unnecessary files

--- dataconnect-generated/js/default-connector/esm/index.esm.js
+++
@@ -1,8 +0,0 @@
-import { , validateArgs } from 'firebase/data-connect';
-
-
-export const connectorConfig = {
-  connector: 'default',
-  service: 'my-collage-bus-tracker',
-  location: 'us-central1'
-};
